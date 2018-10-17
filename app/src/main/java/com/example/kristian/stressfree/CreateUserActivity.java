759dcc0a7266726cf39693d80584a94d15176b54
Bjørn Pedersen <bjoern_94@hotmail.com>
2018-10-17 13:36:05 +0200

    Test for connection 2

--- app/src/main/java/com/example/kristian/stressfree/CreateUserActivity.java
+++ app/src/main/java/com/example/kristian/stressfree/CreateUserActivity.java
@@ -1,13 +1,114 @@
 package com.example.kristian.stressfree;
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class CreateUserActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private String CREATEUSERACTIVITY = "Login  Activity";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+        mAuth = FirebaseAuth.getInstance();
+        MyEditTextDatePicker mydp = new MyEditTextDatePicker(CreateUserActivity.this, R.id.editBirthday);
     }
+
+    public void updateUI(FirebaseUser user){
+
+    }
+
+    public void createAccount (String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(CREATEUSERACTIVITY, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(CREATEUSERACTIVITY, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(CreateUserActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+    // https://stackoverflow.com/questions/14933330/datepicker-how-to-popup-datepicker-when-click-on-edittext
+    // Denne klasse er skrevet af SatanEnglish og rettet af shridutt kothari.
+    public class MyEditTextDatePicker  implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
+        EditText _editText;
+        private int _day;
+        private int _month;
+        private int _birthYear;
+        private Context _context;
+
+        public MyEditTextDatePicker(Context context, int editTextViewID)
+        {
+            Activity act = (Activity)context;
+            this._editText = (EditText)act.findViewById(editTextViewID);
+            this._editText.setOnClickListener(this);
+            this._context = context;
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            _birthYear = year;
+            _month = monthOfYear;
+            _day = dayOfMonth;
+            updateDisplay();
+        }
+        @Override
+        public void onClick(View v) {
+            Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+
+            DatePickerDialog dialog = new DatePickerDialog(_context, this,
+                    calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH));
+            dialog.show();
+
+        }
+
+        // updates the date in the birth date EditText
+        private void updateDisplay() {
+
+            _editText.setText(new StringBuilder()
+                    // Month is 0 based so add 1
+                    .append(_day).append("/").append(_month + 1).append("/").append(_birthYear).append(" "));
+        }
+    }
+
 }
+
+
--- app/src/main/java/com/example/kristian/stressfree/LoginActivity.java
+++ app/src/main/java/com/example/kristian/stressfree/LoginActivity.java
@@ -1,13 +1,42 @@
 package com.example.kristian.stressfree;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private String LOGLOGINACTIVITY = "Login  Activity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
     }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    public void updateUI(FirebaseUser User){
+
+    }
+
+
+
 }
